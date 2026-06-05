719f0a8403cb8394bd840a4c7716e38081b95470
GONG YINUO <25070524@siswa.um.edu.my>
2026-06-06 04:04:16 +0800

    Update BorrowStack.java

--- src/BorrowStack.java
+++ src/BorrowStack.java
@@ -3,13 +3,17 @@ import java.util.Stack;
 public class BorrowStack {
     private Stack<Book> stack;
 
+    // Initialize object
     public BorrowStack(){
         stack = new Stack<>(); 
     }
+    
     /**
      * Pushes a newly borrowed book onto the top of the stack
      */
     public void push(Book book) {
+        
+        // Null check & push book
         if(book != null){
             stack.push(book);
             }
@@ -19,13 +23,15 @@ public class BorrowStack {
      * Iterates backwards through the stack to display books from most recently borrowed to oldest (LIFO order)
      */
     public void show() {
-        //
+        
+        // Empty check
         if(stack.isEmpty()){
             System.out.println("No borrowing history available. ");
             return;
             }
         System.out.println("===== Borrow History (Newest → Oldest) =====");
-        //
+        
+        // Traverse
         for(int i = stack.size()-1; i >= 0; i--){
             Book b = stack.get(i);
             System.out.printf("ISBN: %d | Title: %s | Author: %s%n", b.isbn, b.title, b.author);
