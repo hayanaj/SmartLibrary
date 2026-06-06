07ca1f0aabdc3d461d47285c704da7d90949f0bd
Zoe-ot7 <zoya.habibi29@gmail.com>
2026-06-06 14:36:33 +0800

    Added Book class

--- src/Book.java
+++ src/Book.java
@@ -2,10 +2,17 @@ public class Book {
     int isbn;
     String title;
     String author;
+    
+    // Pointers to the left and right children in the tree
     Book left;
     Book right;
 
+    // Constructor to initialize a new book
     public Book(int isbn, String title, String author) {
-        
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.left = null;
+        this.right = null;
     }
 }
